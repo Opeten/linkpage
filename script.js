b8d7140aa919f6b20eb6574a6a3d91e0fb3fc0ed
Opeten <83654834+Opeten@users.noreply.github.com>
2022-11-12 11:27:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const rawList = `
-    https://api.mod.io/mods/file/3064953
-    https://api.mod.io/mods/file/3060884
-    https://api.mod.io/mods/file/3033580
-    https://api.mod.io/mods/file/3010886
+    https://api.mod.io/v1/games/3809/mods/2403967/files/3124652/download
+https://api.mod.io/v1/games/3809/mods/2405201/files/3124548/download
+https://api.mod.io/v1/games/3809/mods/2389795/files/3082639/download
+https://api.mod.io/v1/games/3809/mods/2448569/files/3082065/download
+https://api.mod.io/v1/games/3809/mods/2480834/files/3124639/download
+https://api.mod.io/v1/games/3809/mods/2421141/files/3118259/download
+https://api.mod.io/v1/games/3809/mods/2442219/files/3073859/download
+https://api.mod.io/v1/games/3809/mods/2401436/files/3042180/download
+https://api.mod.io/v1/games/3809/mods/2424211/files/3050390/download
+https://api.mod.io/v1/games/3809/mods/2448064/files/3081630/download
+https://api.mod.io/v1/games/3809/mods/2413978/files/3120060/download
+https://api.mod.io/v1/games/3809/mods/2469336/files/3122352/download
+https://api.mod.io/v1/games/3809/mods/2406223/files/3018592/download
 `;
 
 
